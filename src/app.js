8bc4d9fd0f1d1da65cf71c137604c4a12f83f7c1
prince-kr-dev <princekumark2004@gmail.com>
2025-07-31 23:36:44 +0530

    exploring GET, PUT, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req, res)=>{
-    res.send("Hello hello hello prince changed");
+//This will only handle GET call to /user
+app.get("/user",(req, res)=>{
+    res.send({firstName: "Prince", lastName: "Kumar"});
 })
 
+
+app.post("/user",(req, res)=>{
+    res.send("Saving data to database");
+})
+
+app.delete("/user",(req, res)=>{
+    res.send("Deleted data successfully");
+})
+
+//This will match all the HTTP method API calls to /test
 app.use("/test",(req, res)=>{
     res.send("Hello from the test server");
 })
