b3f97278d204ba87ef50a38f06918d92244a8e24
prince-kr-dev <princekumark2004@gmail.com>
2025-08-05 23:23:35 +0530

    get user by email

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const {connectDB} = require("./config/database");
+const { connectDB } = require("./config/database");
 const app = express();
-const { User } = require("./models/user")
+const { User } = require("./models/user");
 
 app.use(express.json()); //middelware that convert json to js object
 
@@ -27,6 +27,30 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//GET user by email
+app.get("/user", async (req, res) => {
+  const emailId = req.body.email;
+  try {
+    const user = await User.find({ email: emailId });
+    if (user.length == 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//GET all data
+app.get("/feed", async(req, res) => {
+  try {
+    const user = await User.find({});
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 connectDB()
   .then(() => {
