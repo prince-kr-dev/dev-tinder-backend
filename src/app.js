7b4a0754781489ce88a9412dc3f3f57b1e145470
prince-kr-dev <princekumark2004@gmail.com>
2025-08-05 23:33:37 +0530

    delete user

--- src/app.js
+++ src/app.js
@@ -52,6 +52,17 @@ app.get("/feed", async(req, res) => {
   }
 });
 
+//DELETE user
+app.delete("/user", async(req,res)=>{
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
 connectDB()
   .then(() => {
     console.log("Databse connected successfully");
