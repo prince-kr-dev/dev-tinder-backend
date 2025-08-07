8a9d06c78f4af66f1eb6d8dbd9b98f1c9370f3e8
prince-kr-dev <princekumark2004@gmail.com>
2025-08-07 11:55:04 +0530

    added validators

--- src/models/user.js
+++ src/models/user.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
