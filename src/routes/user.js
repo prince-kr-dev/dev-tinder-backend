9525b13300edc35774613c26ebd4abdefe56cd83
prince-kr-dev <princekumark2004@gmail.com>
2025-08-20 23:22:41 +0530

    created /user/connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,13 +13,11 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
         status: "interested",
       })
       .populate("fromUserId", ["firstName", "lastName", "skills", "photoURL"]);
-      //it is written as string also separated by space
-      //   .populate("fromUserId", "firstName lastName skills"]);
+    //it is written as string also separated by space
+    //   .populate("fromUserId", "firstName lastName skills"]);
 
-    //This excludes mentioned data  
+    //This excludes mentioned data
     // .select("-_id -toUserId -status -__v -createdAt -updatedAt");
-
-    
 
     res.json({
       message: "Data fetched successfully!",
@@ -27,6 +25,35 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await connectionRequestModel
+      .find({
+        $or: [
+          { toUserId: loggedInUser._id, status: "accepted" },
+          { fromUserId: loggedInUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", ["firstName", "lastName", "skills", "photoURL"])
+      .populate("toUserId", ["firstName", "lastName", "skills", "photoURL"]);
+
+      const data = connectionRequests.map(row => {
+        // If logged-in user is the sender, return the recipient
+        if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+          return row.toUserId;
+        }
+        // If logged-in user is the recipient, return the sender
+        return row.fromUserId;
+      });
+
+    res.send({ data });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
