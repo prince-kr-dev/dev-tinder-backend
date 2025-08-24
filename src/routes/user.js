fbc2e0412e434db72aea0448660184059d448779
prince-kr-dev <princekumark2004@gmail.com>
2025-08-24 22:35:08 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -60,6 +60,10 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    limit = limit > 20 ? 20 : limit;
+    const skip = (page - 1) * limit;
     //User should see all the user cards except
     //1. his own card
     //2. his connections
@@ -84,9 +88,12 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     const refinedUserForFeed = await User.find({
       $and: [
         { _id: { $nin: hiddenUserIds } },
-        { _id: { $ne: loggedInUser._id } }
-      ]
-    }).select("firstName lastName skills photoURL");
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select("firstName lastName skills photoURL")
+      .skip(skip)
+      .limit(limit);
 
     res.send(refinedUserForFeed);
   } catch (err) {
