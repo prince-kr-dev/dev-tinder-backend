666e59605e2b48eb5db53512eda3b31afdfa9ac6
prince-kr-dev <princekumark2004@gmail.com>
2025-08-29 00:58:09 +0530

    bug fixed

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -9,7 +9,7 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
   res.send(user);
 });
 
-profileRouter.post("/profile/edit", userAuth, async (req, res) => {
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
     if (!validateEditFields(req)) {
       throw new Error("Invalid Edit request");
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -14,4 +14,23 @@ const validateSignupData = (req)=>{
     }
 }
 
-module.exports = {validateSignupData};
+const validateEditFields = (req) => {
+    const allowedEditFields = [
+      "firstName",
+      "lastName",
+      "emailId",
+      "age",
+      "gender",
+      "about",
+      "photoURL",
+      "skills",
+    ];
+    const isEditAllowed = Object.keys(req.body).every((field) =>
+      allowedEditFields.includes(field)
+    );
+    return isEditAllowed;
+  };
+  module.exports = {
+    validateSignupData,
+    validateEditFields,
+  };
